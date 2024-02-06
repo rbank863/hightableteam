7aafed01d6854936cb93016dd010dcb179701bc7
mpwalke1@asu.edu <mpwalke1@asu.edu>
2024-02-06 15:46:36 -0600

    created Suggestion class object

---
+++ ProjectTemplate/Suggestion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTemplate
+{
+    public class Suggestion
+    {
+        //container to hold all info related to a single suggestion
+
+
+    }
+}
