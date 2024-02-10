ab716128e750f80af01c528b76514855f998e11a
Steven Vo <megavolt@asu.edu>
2024-02-10 15:14:14 -0700

    Added AddComment web service that takes PostID and Comment and inserts into Comments table in DB

--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -241,7 +241,7 @@ namespace ProjectTemplate
             //sets of information, it's a good idea to create a custom container class to represent instances (or rows) of that information, and then return an array of those objects.  
             //Keeps everything simple.
 
-            //WE ONLY SHARE ACCOUNTS WITH LOGGED IN USERS!
+            //WE ONLY SHARE DATA WITH LOGGED IN USERS!
             if (Session["UserID"] != null)
             {
                 DataTable sqlDt = new DataTable("suggestions");
@@ -310,6 +310,43 @@ namespace ProjectTemplate
                 //if they're not logged in, return an empty array
                 return new Suggestion[0];
             }
+        }
+
+        [WebMethod(EnableSession = true)]
+        public void AddComment(string postId, string comment)
+        {
+            string sqlConnectString = getConString();
+            //the only thing fancy about this query is SELECT LAST_INSERT_ID() at the end.  All that
+            //does is tell mySql server to return the primary key of the last inserted row.
+            string sqlSelect = "insert into Comments (EmpID, PostID, Comment, Date) " +
+                "values(@empIdValue, @postIdValue, @commentValue, CURRENT_TIMESTAMP); SELECT LAST_INSERT_ID();";
+
+            MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+            sqlCommand.Parameters.AddWithValue("@empIdValue", Convert.ToInt32(Session["EmpID"]));
+            sqlCommand.Parameters.AddWithValue("@postIdValue", HttpUtility.UrlDecode(postId));
+            sqlCommand.Parameters.AddWithValue("@commentValue", HttpUtility.UrlDecode(comment));
+
+            //this time, we're not using a data adapter to fill a data table.  We're just
+            //opening the connection, telling our command to "executescalar" which says basically
+            //execute the query and just hand me back the number the query returns (the ID, remember?).
+            //don't forget to close the connection!
+            sqlConnection.Open();
+            //we're using a try/catch so that if the query errors out we can handle it gracefully
+            //by closing the connection and moving on
+            try
+            {
+                int accountID = Convert.ToInt32(sqlCommand.ExecuteScalar());
+                //here, you could use this accountID for additional queries regarding
+                //the requested account.  Really this is just an example to show you
+                //a query where you get the primary key of the inserted row back from
+                //the database!
+            }
+            catch (Exception e)
+            {
+            }
+            sqlConnection.Close();
         }
 
     }
